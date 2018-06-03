4053a36b0db80b27cb2b9b746ef6083aa53ec143
QinChen686 <1403453010@qq.com>
2018-06-03 19:23:44 +0800

    complete MgrManagerImpl.java

--- WEB-INF/src/org/qc/hrsystem/service/impl/MgrManagerImpl.java
+++ WEB-INF/src/org/qc/hrsystem/service/impl/MgrManagerImpl.java
@@ -4,6 +4,8 @@ import org.qc.hrsystem.service.MgrManager;
 import org.qc.hrsystem.exception.HrException;
 import org.qc.hrsystem.domain.*;
 import org.qc.hrsystem.vo.*;
+
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 
@@ -101,26 +103,109 @@ public class MgrManagerImpl implements MgrManager
 
     public void check(int appId, String Mgr, boolean result) throws HrException
     {
+        Application app=appDao.get(Application.class, appId);
+        CheckBack check=new CheckBack();
         Manager mgr=mgrDao.findByName(Mgr);
         if(mgr==null)
         {
             throw new HrException("你是经理吗，或你还未登录？");
         }
-        Application app=appDao.get(Application.class, appId);
+        check.setManager(mgr);
+        check.setApp(app);
+        if(result)
+        {
+            check.setResult(true);
+            app.setResult(true);
+            appDao.update(app);
+            Attend att=app.getAttend();
+            att.setType(app.getType());
+            attendDao.update(att);
+        }
+        else {
+            check.setResult(false);
+            app.setResult(true);
+            appDao.update(app);
+        }
+        checkDao.save(check);
     }
 
     public List<AppBean> getAppByMgr(String mgr) throws HrException
     {
+        Manager manager=mgrDao.findByName(mgr);
+        if(manager==null)
+        {
+            throw new HrException("你不是经理，或你还未登录");
+        }
+        Set<Employee> emps=manager.getEmployees();
+        if(emps==null || emps.size()<1)
+        {
+            throw new HrException("你还没有员工！");
+        }
+        List<AppBean> appBeans=new ArrayList<AppBean>();
+        for(Employee emp:emps)
+        {
+            List<Application> apps=appDao.findByEmp(emp);
+            if(apps!=null || apps.size()>=1)
+            {
+                for(Application app:apps)
+                {
+                    if(app.getResult()==false)
+                    {
+                        appBeans.add(new AppBean(app.getId(),emp.getName(),app.getAttend().getType().getName(),app.getType().getName(),app.getReason()));
+                    }
 
+                }
+            }
+        }
+        return appBeans;
     }
+
 
     public List<EmpBean> getEmpByMgr(String mgr) throws HrException
     {
-
+        Manager manager = mgrDao.findByName(mgr);
+        if(manager==null)
+        {
+            throw new HrException("你不是经理或你还未登录");
+        }
+        List<EmpBean> empBeans = new ArrayList<>();
+        Set<Employee> emps = manager.getEmployees();
+        if(emps==null||emps.size()<1)
+        {
+            throw new HrException("你的部门没有员工");
+        }
+        for(Employee emp:emps)
+        {
+            empBeans.add(new EmpBean(emp.getName(),emp.getPass(),emp.getSalary()));
+        }
+        return empBeans;
     }
 
-    public List<SalaryBean> getSalaryByMgr(String mgr) throws HrException
-    {
-
+    /**该经理员工上个月的薪水
+     *
+     * @param mgr 经理名
+     * @return 所有员工的上个月薪水
+     * @throws HrException 输入的经理名不是经理的异常
+     */
+    public List<SalaryBean> getSalaryByMgr(String mgr) throws HrException {
+        Manager manager = mgrDao.findByName(mgr);
+        if (manager == null) {
+            throw new HrException("你不是经理或你还未登录？");
+        }
+        Set<Employee> emps = manager.getEmployees();
+        if (emps == null || emps.size() < 1)
+        {
+            throw new HrException("你的部门没有员工");
+        }
+        Calendar c=Calendar.getInstance();
+        SimpleDateFormat sft=new SimpleDateFormat("yyyy-MM");
+        c.add(Calendar.MONTH,-1);
+        String month=sft.format(c.getTime());
+        List<SalaryBean> salaryBeans = new ArrayList<> ();
+        for(Employee emp:emps)
+        {
+            salaryBeans.add(new SalaryBean(emp.getName(),emp.getSalary()));
+        }
+        return salaryBeans;
     }
 }
